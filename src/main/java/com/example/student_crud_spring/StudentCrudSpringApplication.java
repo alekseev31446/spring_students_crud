72b2f328ba898f10ef9917d323dfe2b1e63f83f4
alekseev31446 <alekseev3446@gmail.com>
2023-11-11 00:50:43 +0200

    initial commit

---
+++ src/main/java/com/example/student_crud_spring/StudentCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_crud_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/student_crud_spring/StudentCrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_crud_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
